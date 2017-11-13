0c8bf10a49feb862ea8c0a8ca18ca5d0c2dac6c9
billy mitchell <william.mitchell@hmrc.gsi.gov.uk>
2017-11-13 09:42:27 +0000

    Formatting the maze

--- src/View.elm
+++ src/View.elm
@@ -8,6 +8,24 @@ import Maze exposing (Boundary(..), Cell, Direction(..), Maze)
 import Model exposing (..)
 
 
+mainContainer : Html.Attribute msg
+mainContainer =
+    style
+        [ ( "backgroundColor", "" )
+        , ( "width", "auto" )
+        , ( "padding", "25px" )
+        , ( "position", "center" )
+        , ( "text-align", "center" )
+        , ( "margin", "auto" )
+        ]
+
+
+buttonStyler : Html.Attribute msg
+buttonStyler =
+    style
+        [ ( "padding", "25px" ) ]
+
+
 drawRow : Array Cell -> List String
 drawRow cs =
     let
@@ -66,7 +84,9 @@ drawMaze maze =
 
 view : Model -> Html Message
 view model =
-    div [] [ mazeView model, buttonView model ]
+    div []
+        [ div [ mainContainer ] [ mazeView model, buttonView model ]
+        ]
 
 
 buttonView : Model -> Html Message
@@ -75,7 +95,7 @@ buttonView model =
         smallable =
             not (model.rowNo <= 5 || model.colNo <= 5)
     in
-    div []
+    div [ buttonStyler ]
         [ button [ onClick Next ] [ text "Next" ]
         , button [ onClick Bigger ] [ text "Bigger" ]
         , button [ onClick Smaller, disabled (not smallable) ] [ text "Smaller" ]
