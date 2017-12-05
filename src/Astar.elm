4497aba706e2fbd4c18c8fa8ca2803912857e727
Rose Garratt <rose.garratt@digital.hmrc.gov.uk>
2017-12-05 10:08:02 +0000

    Improve AStar

--- src/Astar.elm
+++ src/Astar.elm
@@ -1,13 +1,61 @@
-module Astar exposing (..)
+module AStar exposing (..)
 
-import Grid exposing (Grid, Position, height, width)
-import Maze exposing (Maze)
+import Grid exposing (Grid, Position, create, height, width)
+import Maze exposing (Maze, validNeighbours)
 
 
-colorMaze : Maze -> Position -> Grid (Maybe Int)
-colorMaze maze start =
+type alias Distance =
+    Int
+
+
+type alias Distances =
+    Grid (Maybe Distance)
+
+
+breadthFirst : Maze -> Position -> Distances
+breadthFirst maze start =
     let
+        h =
+            height maze
+
+        w =
+            width maze
+
         maxDistance =
-            height maze * width maze
+            h * w
+
+        g =
+            Grid.create h w (Just maxDistance)
+                |> Grid.set ( 0, 0 ) (Just 0)
+                |> Grid.set ( 1, 1 ) (Just (maxDistance // 2))
+
+        --get start pos, set dist as 0, get neighbours set as +1
+        --current cost neighbours to visit carry this on spreading through grid carry answer through grid
     in
-    Grid.empty
+    g
+
+
+setDistanceOfNeighbours : Maze -> ( List Position, Distance, Distances ) -> ( List Position, Distance, Distances )
+setDistanceOfNeighbours maze ( currentPositions, distance, distances ) =
+    let
+        isDistanceless : Position -> Bool
+        isDistanceless position =
+            Grid.get position distances
+                |> Maybe.Extra.join
+                |> Maybe.Extra.isNothing
+
+        applyDistance : Position -> Distances -> Distances
+        applyDistance position distances =
+            Grid.set position distance distances
+
+        --invoke validNeighbours maze on ever element of current positions (and flatten it using concatMap)
+        currentNeighbours =
+            List.concatMap (Maze.validNeighbours maze) currentPositions
+
+        awesomeNeighbours =
+            List.filter isDistanceless currentNeighbours
+
+        newDistances =
+            List.foldl applyDistance distances awesomeNeighbours
+    in
+    ( awesomeNeighbours, distance + 1, newDistances )
--- src/View.elm
+++ src/View.elm
@@ -1,7 +1,9 @@
 module View exposing (view)
 
+import AStar
 import Array exposing (Array)
-import Grid exposing (Position)
+import Color exposing (Color)
+import Grid exposing (Grid, Position)
 import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
@@ -12,45 +14,29 @@ import Svg as S exposing (Svg)
 import Svg.Attributes as SA
 
 
-mainContainer : Html.Attribute msg
-mainContainer =
-    style
-        [ ( "backgroundColor", "" )
-        , ( "width", "auto" )
-        , ( "padding", "25px" )
-        , ( "position", "center" )
-        , ( "text-align", "center" )
-        , ( "margin", "auto" )
-        ]
+type alias Coordinate =
+    { x : Int, y : Int }
 
 
-buttonStyler : Html.Attribute msg
-buttonStyler =
-    style
-        [ ( "padding", "25px" ) ]
+asCoordinate : Position -> Int -> Coordinate
+asCoordinate ( row, col ) scale =
+    { x = col * scale, y = row * scale }
 
 
 view : Model -> Html Message
 view model =
-    div [] [ div [ mainContainer ] [ drawLineyMaze model.maze, buttonView model, test model.maze ] ]
-
-
-test : Maze -> Html msg
-test maze =
     let
-        -- position =
-        --     ( 0, 0 )
-        --
-        -- canIMove : Direction -> Bool
-        -- canIMove =
-        --     Maze.canIMove maze position
-        -- dir =
-        --     [ canIMove North, canIMove South, canIMove East, canIMove West ]
-        dir =
-            Maze.validDirections maze ( 1, 1 )
+        mainContainerStyle =
+            style
+                [ ( "backgroundColor", "" )
+                , ( "width", "auto" )
+                , ( "padding", "25px" )
+                , ( "position", "center" )
+                , ( "text-align", "center" )
+                , ( "margin", "auto" )
+                ]
     in
-        toString dir
-            |> text
+    div [] [ div [ mainContainerStyle ] [ drawLineyMaze model.maze, buttonView model ] ]
 
 
 drawNorthWall : Int -> Coordinate -> S.Svg msg
@@ -65,7 +51,37 @@ drawEastWall scale { x, y } =
 
 drawWall : Coordinate -> Coordinate -> S.Svg msg
 drawWall c1 c2 =
-    S.line [ SA.x1 (toString c1.x), SA.y1 (toString c1.y), SA.x2 (toString c2.x), SA.y2 (toString c2.y), SA.style "stroke:rgb(255,0,0);stroke-width:2" ] []
+    S.line [ SA.x1 (toString c1.x), SA.y1 (toString c1.y), SA.x2 (toString c2.x), SA.y2 (toString c2.y), SA.style "stroke:black;stroke-linecap:round;stroke-width:3" ] []
+
+
+colorBlock : Int -> Coordinate -> Color -> S.Svg msg
+colorBlock scale coord color =
+    let
+        fillStyle =
+            "fill:" ++ colorToHtmlRgb color
+
+        _ =
+            Debug.log "Style = " fillStyle
+    in
+    S.rect [ SA.x (toString coord.x), SA.y (toString coord.y), SA.width (toString scale), SA.height (toString scale), SA.style fillStyle ] []
+
+
+colorToHtmlRgb : Color -> String
+colorToHtmlRgb color =
+    let
+        rgb =
+            Color.toRgb color
+    in
+    "rgb(" ++ toString rgb.red ++ "," ++ toString rgb.green ++ "," ++ toString rgb.blue ++ ")"
+
+
+colorCell : Int -> Position -> Color -> S.Svg msg
+colorCell scale position color =
+    let
+        coordinateOfCell =
+            asCoordinate position scale
+    in
+    colorBlock scale coordinateOfCell color
 
 
 drawLineyMaze : Maze -> Html msg
@@ -81,9 +97,8 @@ drawLineyMaze maze =
         rows =
             Grid.height maze
 
-        fn : ( Position, Cell ) -> List (S.Svg a)
-        fn ( position, cell ) =
-            drawCell scale position cell
+        cg =
+            colorGrid ( rows // 2, cols // 2 ) maze
 
         width =
             cols
@@ -104,18 +119,41 @@ drawLineyMaze maze =
         asHtml : List (S.Svg msg) -> Html msg
         asHtml svgMsgs =
             S.svg [ SA.width width, SA.height height, SA.viewBox viewbox ] svgMsgs
+
+        cellBorders =
+            let
+                drawCellBoundaries : Position -> Cell -> List (S.Svg a)
+                drawCellBoundaries position cell =
+                    drawCell scale position cell
+            in
+            Grid.indexedMap drawCellBoundaries maze
+                |> Grid.toList
+                |> List.concat
+
+        cellColors =
+            let
+                drawBackroundColor : Position -> Cell -> List (S.Svg a)
+                drawBackroundColor position _ =
+                    let
+                        color =
+                            Grid.get position cg
+                    in
+                    Maybe.map (\c -> [ colorCell scale position c ]) color
+                        |> Maybe.withDefault []
+            in
+            Grid.indexedMap drawBackroundColor maze
+                |> Grid.toList
+                |> List.concat
+
+        boundaries =
+            drawBoundary scale rows cols
     in
-        Grid.indexedMap gridToCellNPosition maze
-            |> Grid.map fn
-            |> Grid.toList
-            |> List.concat
-            |> List.append (drawBoundary scale rows cols)
-            |> asHtml
-
-
-gridToCellNPosition : Position -> Cell -> ( Position, Cell )
-gridToCellNPosition position cell =
-    ( position, cell )
+    List.concat
+        [ cellColors
+        , cellBorders
+        , boundaries
+        ]
+        |> asHtml
 
 
 drawBoundary : Int -> Int -> Int -> List (S.Svg msg)
@@ -127,7 +165,7 @@ drawBoundary scale rows cols =
         actualWidth =
             scale * cols
     in
-        [ drawWall { x = 0, y = 0 } { x = 0, y = actualHeight }, drawWall { x = 0, y = actualHeight } { x = actualWidth, y = actualHeight } ]
+    [ drawWall { x = 0, y = 0 } { x = 0, y = actualHeight }, drawWall { x = 0, y = actualHeight } { x = actualWidth, y = actualHeight } ]
 
 
 drawCell : Int -> Position -> Cell -> List (S.Svg msg)
@@ -148,37 +186,69 @@ drawCell scale position cell =
         drawEastWithScaleAndCoords =
             drawEastWall scale coordinateOfCell
     in
-        case ( isNorthWall, isEastWall ) of
-            ( True, True ) ->
-                [ drawNorthWithScaleAndCoords, drawEastWithScaleAndCoords ]
+    case ( isNorthWall, isEastWall ) of
+        ( True, True ) ->
+            [ drawNorthWithScaleAndCoords, drawEastWithScaleAndCoords ]
 
-            ( True, False ) ->
-                [ drawNorthWithScaleAndCoords ]
+        ( True, False ) ->
+            [ drawNorthWithScaleAndCoords ]
 
-            ( False, True ) ->
-                [ drawEastWithScaleAndCoords ]
+        ( False, True ) ->
+            [ drawEastWithScaleAndCoords ]
 
-            ( False, False ) ->
-                []
-
-
-type alias Coordinate =
-    { x : Int, y : Int }
-
-
-asCoordinate : Position -> Int -> Coordinate
-asCoordinate ( row, col ) scale =
-    { x = col * scale, y = row * scale }
+        ( False, False ) ->
+            []
 
 
 buttonView : Model -> Html Message
 buttonView model =
     let
+        buttonStyle =
+            style
+                [ ( "padding", "25px" ) ]
+
         smallable =
             not (model.rowNo <= 5 || model.colNo <= 5)
     in
-        div [ buttonStyler ]
-            [ button [ onClick Next ] [ text "Next" ]
-            , button [ onClick Bigger ] [ text "Bigger" ]
-            , button [ onClick Smaller, disabled (not smallable) ] [ text "Smaller" ]
-            ]
+    div [ buttonStyle ]
+        [ button [ onClick Next ] [ text "Next" ]
+        , button [ onClick Bigger ] [ text "Bigger" ]
+        , button [ onClick Smaller, disabled (not smallable) ] [ text "Smaller" ]
+        ]
+
+
+colorGrid : Position -> Maze -> Grid.Grid Color
+colorGrid origin maze =
+    let
+        absMax =
+            Grid.height maze * Grid.width maze
+
+        dists : Grid.Grid Int
+        dists =
+            AStar.breadthFirst maze origin
+                |> Grid.map (Maybe.withDefault absMax)
+
+        maxDist =
+            dists
+                |> Grid.toList
+                |> List.maximum
+                |> Maybe.withDefault absMax
+
+        shade : Int -> Color
+        shade dist =
+            let
+                intensity : Float
+                intensity =
+                    toFloat (maxDist - dist) / toFloat maxDist
+
+                dark : Int
+                dark =
+                    (255.0 * intensity) |> round
+
+                bright : Int
+                bright =
+                    128 + (128.0 * intensity |> round)
+            in
+            Color.rgb dark bright dark
+    in
+    Grid.map shade dists
